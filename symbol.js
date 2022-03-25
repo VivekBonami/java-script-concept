a6b521b50c6c425f4a3021a7bc414606f241a654
VivekBonami <vivek.kumar@bonamisoftware.com>
2022-03-25 17:51:29 +0530

    symbol

---
+++ symbol.js
@@ -0,0 +1,14 @@
+const k1 = Symbol()
+const k2 = Symbol()
+
+myobj = {}
+
+myobj[k1] =     "Harry"
+myobj[k2] =     "Rohan"
+
+console.log(myobj)
+console.log(myobj.k1)
+console.log(myobj.k2)
+
+// Every symbol is differnt 
+// symbol is used make a unique idenfier if value is same still they won't be equal
